e578bec6b4eeff694c70e88719c0ec6bf958cb62
Rodrigo <rodrigofelix.leite@gmail.com>
2021-01-10 23:15:49 -0300

    version 1.0

--- js/script.js
+++ js/script.js
@@ -8,15 +8,15 @@ const gameArea = {
     crocs: [],
     score: 0,
     drawScore: function() {
-        ctx.font = '18px tahoma';
-        ctx.fillStyle = 'white';
-        ctx.fillText(`Score ${this.score}`, 165, 650);
+        ctx.font = '25px arial';
+        ctx.fillStyle = 'white'
+        ctx.fillText(`Score ${this.score} |`, 20, 670);
+        ctx.fillText(`Your target is: 200`, 170, 670);
     },
     start: function (){
         this.interval = setInterval(updateArea, 20);
     }
 };
-
 
 class Background {
     constructor(){
@@ -25,12 +25,14 @@ class Background {
         this.width = canvas.width
         this.height = canvas.height
         this.land = 200
+        this.imgLand = new Image()
+        this.imgLand.src = './styles/sand.png'
+        this.imgWater = new Image()
+        this.imgWater.src = './styles/water.png'
     }
     draw(){
-        ctx.fillStyle = 'blue';
-        ctx.fillRect(this.x, this.y, this.width, this.height-this.land);
-        ctx.fillStyle = 'brown';
-        ctx.fillRect(this.x, this.height-this.land, this.width, this.land)
+        ctx.drawImage(this.imgWater, this.x, this.y, this.width, this.height-this.land);
+        ctx.drawImage(this.imgLand, this.x, this.height-this.land, this.width, this.land);
     }
 }
 
@@ -47,10 +49,13 @@ class Croc {
         this.y = posY
         this.width = 50
         this.height = 50
+        this.img = new Image()
+        this.img.src = './styles/croc.png'
     }
     draw(){
-        ctx.fillStyle = 'green';
-        ctx.fillRect(this.x, this.y, this.width, this.height)
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height)
+        // ctx.fillStyle = 'green';
+        // ctx.fillRect(this.x, this.y, this.width, this.height)
     }
     move(){
         this.y = this.y + gameArea.speed
@@ -96,12 +101,47 @@ canvas.addEventListener('mouseup', e => {
 });
 
 function validationAttack(){
-    gameArea.crocs.forEach(croc => {
-        if (y >= croc.y && y <= croc.y+50 && x >= croc.x && x <= croc.x+50){
-            gameArea.score += 10;
+    for (let i = 0; i < gameArea.crocs.length; i++) {
+        if( y >= gameArea.crocs[i].y && y <= gameArea.crocs[i].y+50 && x >= gameArea.crocs[i].x && x <= gameArea.crocs[i].x+50){
+            gameArea.score += 10
+            gameArea.crocs.splice(i, 1)
+            gameArea.speed += 0.1
         }
-    })
+    }
+    // gameArea.crocs.forEach((croc, index) => {
+    //     if (y >= croc.y && y <= croc.y+50 && x >= croc.x && x <= croc.x+50){
+    //         gameArea.crocs.splice(index, 1)
+    //         gameArea.score += 10
+    // )}
 }
+
+function attackDone(){
+    for (let i = 0; i < gameArea.crocs.length; i++) {
+        if( gameArea.crocs[i].y > 550){
+            const imgGameOver = new Image()
+            imgGameOver.src = './styles/gameover.PNG'
+            clearInterval(gameArea.interval)
+            clearArea()
+            imgGameOver.onload = function(){
+                ctx.drawImage(imgGameOver, 0, 0)
+            }
+            console.log(imgGameOver)
+        }
+    }
+}
+
+function success(){
+    if( gameArea.score === 200){
+            const imgSuccess = new Image()
+            imgSuccess.src = './styles/success.PNG'
+            clearInterval(gameArea.interval)
+            clearArea()
+            imgSuccess.onload = function(){
+                ctx.drawImage(imgSuccess, 0, 0)
+            }
+            console.log(imgSuccess)
+        }
+    }
 
 function updateArea() {
     gameArea.frames += 1
@@ -109,8 +149,9 @@ function updateArea() {
     background.draw();
     createCrocs();
     moveCrocs();
-    gameArea.drawScore();
-    console.log(gameArea.score)
+    success();
+    attackDone();
+    gameArea.drawScore()
 }
 
 gameArea.start()
